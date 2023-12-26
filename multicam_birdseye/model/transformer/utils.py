11965793b12b3c75ed4da773bf92ef424143974b
Renu <68454938+RenuReddyK@users.noreply.github.com>
2023-12-26 17:16:20 -0500

    Add files via upload

---
+++ multicam_birdseye/model/transformer/utils.py
@@ -0,0 +1,11 @@
+'''
+Author: Ayush Goel (aygoel@seas.upenn.edu)
+'''
+import tensorflow
+
+def K_meshgrid(x, y):
+    return tensorflow.meshgrid(x, y)
+
+
+def K_linspace(start, stop, num):
+    return tensorflow.linspace(start, stop, num)
